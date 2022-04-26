eb7b7b4e0fa3eb8d01f9372f6a5c7d9a64686f93
Nehakumari-pawar <nehakumari.pawar@fissionlabs.com>
2022-04-26 16:21:41 +0530

    clearedLink and added reuable components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line prettier/prettier
 module.exports = {
   root: true,
   extends: '@react-native-community',
---
+++ src/Screens/RegisterScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Alert} from 'react-native';
+import Register from './Register';
+// import {Context} from '../../context/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const CreateScreen = ({navigation}) => {
+  // const {register} = useContext(Context);
+
+  const onSubmitHandler = async (username, email, password) => {
+    try {
+      if (email === '' || password === '' || username === '') {
+        // condition to ckeck weather the field are empty or not
+        Alert.alert('Please Fill all the Fields');
+      }
+      //    register(username,email,password, ()=>{
+      //      navigation.navigate("Login")
+      //   })
+      else {
+        Alert.alert('Thank you for Registering'); // if the fields are not empty user will navigated to login page
+        navigation.navigate('Login');
+      }
+
+      await AsyncStorage.setItem('email', JSON.stringify(email)); //storing the email data in asyncStorage
+      await AsyncStorage.setItem('password', JSON.stringify(password)); //storing the password in asyncStorage
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  return <Register onSubmit={onSubmitHandler} />;
+};
+
+export default CreateScreen;
